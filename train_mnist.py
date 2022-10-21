91b316c07b5002bb5c2f1755839b6c53c33b3744
Leo Kosta <kostaleonard@gmail.com>
2022-10-21 10:57:45 -0400

    Added GPU configuration
    Signed-off-by: Leo Kosta <kostaleonard@gmail.com>

--- train_mnist.py
+++ train_mnist.py
@@ -7,10 +7,17 @@ from tensorflow.keras.layers import Dense, Flatten
 
 MNIST_IMAGE_SHAPE = (28, 28)
 MAX_PIXEL_VALUE = 255
+BATCH_SIZE = 4096
 
-device = tf.config.list_physical_devices('GPU')
-tf.config.experimental.set_memory_growth(device[0], True)
-tf.config.experimental.set_virtual_device_configuration(device[0], [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=1024)])
+# TODO explain this in issue
+# https://forums.developer.nvidia.com/t/jetson-nano-running-out-of-memory-resourceexhaustederror-oom-when-allocating-tensor-with-shape-3-3-512-1024/154513/5
+devices = tf.config.list_physical_devices('GPU')
+if devices:
+    tf.config.experimental.set_memory_growth(devices[0], True)
+    tf.config.experimental.set_virtual_device_configuration(devices[0], [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=1024)])
+
+# TODO log GPU stats if possible
+# TODO use a TF dataset or something to repeat the dataset and show speedups? Or is that too complicated for an MNIST example?
 
 
 def main() -> None:
@@ -26,15 +33,15 @@ def main() -> None:
     print(X_test.shape)
     print(y_test.shape)
     model = Sequential([
-        Flatten(input_shape=(MNIST_IMAGE_SHAPE), name='flatten'),
-        Dense(128, activation='relu', name='dense_1'),
-        Dense(10, activation='softmax', name='dense_2')
+        Flatten(input_shape=MNIST_IMAGE_SHAPE, name='flatten'),
+        Dense(128, activation='relu'),
+        Dense(10, activation='softmax')
     ])
     model.compile(optimizer='adam',
                   loss='sparse_categorical_crossentropy',
                   metrics=['sparse_categorical_accuracy'])
-    model.fit(X_train, y_train, epochs=10, batch_size=32)
-    metrics = model.evaluate(X_test, y_test)
+    model.fit(X_train, y_train, epochs=10, batch_size=BATCH_SIZE)
+    metrics = model.evaluate(X_test, y_test, batch_size=BATCH_SIZE)
     print(metrics)
 
 
